4328626c8993fc6980e7e6b7e053ad422582ebc8
xiaoming <xiaomingboy0215.163.com>
2017-06-29 13:18:17 +0800

    'WIP'

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import FilterableProductTable from './components/FilterableProductTable';
+import List from './components/List';
 import './app.scss';
 
 let root = document.getElementById('app');
@@ -10,17 +10,21 @@ if (!root) {
     document.body.appendChild(root);
 }
 
-const PRODUCTS = [
-    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-];
+// const PRODUCTS = [
+//     { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+//     { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+//     { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+//     { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+//     { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+//     { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+// ];
 
+// ReactDOM.render(
+//     <FilterableProductTable products={PRODUCTS} />,
+//     document.getElementById('app')
+// );
 ReactDOM.render(
-    <FilterableProductTable products={PRODUCTS} />,
+    <List />,
     document.getElementById('app')
 );
 
